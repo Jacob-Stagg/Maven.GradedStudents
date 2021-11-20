58d88578755dfe59e9d7e307b7efd126aa0a843a
Stagg Jacob <staggjacob@zipcodes-MacBook-Pro-2.local>
2021-11-20 14:21:19 -0500

    70% done

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,37 @@
 package io.zipcoder;
 
 public class Classroom {
+    private Student[] students;
+    private Integer maxNumberOfStudents;
+
+    public Classroom(Integer maxNumberOfStudents) {
+        this.maxNumberOfStudents = maxNumberOfStudents;
+        this.students = new Student[maxNumberOfStudents];
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public Double getAverageExamScore() {
+        String examScore = "";
+
+        for (Student student : students) {
+            examScore += student;
+        }
+
+        Double averageExamScore = Double.parseDouble(examScore);
+
+        return averageExamScore / students.length;
+    }
+
+    public String addStudent(Student student) {
+        for (int i = 0; i < students.length; i++) {
+            students[i] = student;
+        }
+
+        return "";
+    }
+
+
 }
